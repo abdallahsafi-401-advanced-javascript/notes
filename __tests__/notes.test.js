05fc6c74e3c08a2efa128675921162d911375ef2
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-09-09 02:00:24 +0300

    lab3 complete

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('@code-fellows/supergoose');
 const Notes = require('../lib/notes.js');
 
 // Spies!
@@ -8,29 +9,58 @@ jest.spyOn(global.console, 'log');
 
 // describe the module I am testing
 describe('Notes Module', () => {
-  // test case
-
-  it('add() does nothing with invalid options', () => {
+  // test add
+  it('add() should save the note to database', () => {
+    let data = { payload: 'note', category: 'test' };
     const notes = new Notes();
-    notes.add(undefined);
-    expect(console.log).not.toHaveBeenCalled();
+    return notes.add(data).then((res) => {
+      for (const key in data) {
+        expect(res[key]).toEqual(data[key]);
+      }
+    });
   });
 
-  it('add() does nothing with invalid options', () => {
+  // test delete
+  it('delete() should delete the note from database', () => {
+    let data = { payload: 'note', category: 'test' };
     const notes = new Notes();
-    notes.add();
-    expect(console.log).not.toHaveBeenCalled();
+    return notes.add(data).then((res) => {
+      return notes.delete({ payload: res._id }).then((result) => {
+        for (const key in data) {
+          expect(result[key]).toEqual(data[key]);
+        }
+      });
+    });
   });
 
-  it('add() logs out when given options', () => {
+  //test list
+  it('list() should get me the list of notes in database', () => {
+    let data = { payload: 'note', category: 'test' };
     const notes = new Notes();
-    notes.add('note');
-    expect(console.log).toHaveBeenCalled();
+    return notes.add(data).then((res) => {
+      return notes.list({ payload: 'test' }).then((result) => {
+        for (const key in data) {
+          expect(result[0][key]).toEqual(data[key]);
+        }
+      });
+    });
   });
 
+  // console log in execute
   it('execute() logs out when given options', () => {
     const notes = new Notes();
     notes.execute({ action: undefined });
     expect(console.log).toHaveBeenCalled();
   });
+
+  // Check for console log
+  it('add() logs out when given options', () => {
+    let data = { payload: 'note', category: 'test' };
+    const notes = new Notes();
+    return notes.add(data).then((res) => {
+      expect(console.log).toHaveBeenCalled();
+    });
+  });
+
+
 });
